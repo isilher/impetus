76bd044938135d6702c239a05b222f6c7370c27e
Rik van Duijn <ik@rikvanduijn.nl>
2022-07-25 13:40:55 +0200

    Update toc linting config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 // This enables ESLint to use dependencies of this config
 // (see https://github.com/eslint/eslint/issues/3458)
-require('eslint-config-toc/setupPlugins');
+require('eslint-config-toc/setup-plugins');
 
 module.exports = {
   // Add configs based on your needs
